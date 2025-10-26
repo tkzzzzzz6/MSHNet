4d391a8ee842868b1debd5bceb8371d5ac5402fd
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2025-10-26 21:52:28 +0800

    Update batch size in run.sh from 4 to 64 for training configuration

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # conda env create -f environment.yaml
 # conda activate mshnet
-python main.py --dataset-dir './datasets/IRSTD-1k' --batch-size 4 --epochs 400 --lr 0.05 --mode train
+python main.py --dataset-dir './datasets/IRSTD-1k' --batch-size 64 --epochs 400 --lr 0.05 --mode train
